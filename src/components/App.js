c2bb8f37dc9c5fd96a8b9a3a806915d3924a3e6e
Kyle Potter <kp1289@gmail.com>
2020-09-23 21:44:32 +0100

    added call to youtube api

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube";
 
 class App extends React.Component {
+   onTermSubmit = async (term) => {
+    let videos = await youtube.get("/search", {
+      params: {
+        q: term,
+      },
+    });
+  };
   render() {
     return (
       <div style={{ paddingTop: "15px" }} className="ui container">
-        <SearchBar />
+        <SearchBar onFormSubmit={this.onTermSubmit} />
       </div>
     );
   }
