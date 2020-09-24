8d5bdd3122cd7cf85200cab8f0ebb309522b2385
Kyle Potter <kp1289@gmail.com>
2020-09-24 21:56:15 +0100

    formatted image card further

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,8 +1,9 @@
+import './VideoItem.css'
 import React from "react";
 
 function VideoItem({ video }) {
   return (
-    <div className="item">
+    <div className="video-item item">
       <img className="ui image" src={video.snippet.thumbnails.medium.url} alt="thumbnail" />
       <div className="content">
         <div className="header">{video.snippet.title}</div>
